c93f8f2be9c1cbc7e6471f016bb499f8dc2b9f66
21Chani <dmendoncacosta03@gmail.com>
2023-06-02 23:09:48 -0300

    feat(ubuntu): remove unnecessary PS1

--- ubuntu/default_config.sh
+++ ubuntu/default_config.sh
@@ -1,4 +1,6 @@
 PROMPT_DIRTRIM=1
+# see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
+# for examples
 
 # If not running interactively, don't do anything
 case $- in
@@ -49,11 +51,6 @@ if [ -n "$force_color_prompt" ]; then
     fi
 fi
 
-if [ "$color_prompt" = yes ]; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ '
-else
-    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
-fi
 unset color_prompt force_color_prompt
 
 
