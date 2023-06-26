9846e04702d9e8777de49be09a3719502a7f21fa
21Chani <dmendoncacosta03@gmail.com>
2023-06-26 08:43:55 -0300

    feat: install gnome tweaks and shell extensions

--- ubuntu/install/tools.sh
+++ ubuntu/install/tools.sh
@@ -1,17 +1,33 @@
 #/bin/bash!
 
-# INSTALL CURL ------------------------------
+# INSTALL CURL -----------------------
 if ! command -v curl &>/dev/null; then
     echo installing curl.
     sudo apt install curl -y
 else
-    echo " - curl already installed, skipping."
+    echo " - Curl already installed, skipping."
 fi
 
-# INSTALL RUSTUP --------------------------------------------------
+# INSTALL RUSTUP -----------------------
 if ! command -v rustup &>/dev/null; then
     echo "installing rustup. \n"
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 else
-    echo " - rustup already installed, skipping."
+    echo " - Rustup already installed, skipping."
+fi
+
+# INSTALL GNOME TWEAKS -----------------------
+if ! command -v gnome-tweaks &>/dev/null; then
+    echo "installing gnome-tweaks"
+    sudo apt install gnome-tweaks
+else
+    echo " - Gnome Tweaks already installed, skipping."
+fi
+
+# INSTALL GNOME EXTENSIONS ------------------------
+if ! dpkg -s gnome-shell-extensions >/dev/null 2>&1; then
+    echo "insalling gnome-tweaks"
+    sudo apt install gnome-tweaks
+else
+    echo " - Gnome Shell Extensions already installed, skipping."
 fi
